2a1468a192a8d79386c1d2f0fe920005b10c0e97
theDrake1010 <40403909+theDrake1010@users.noreply.github.com>
2019-09-18 22:53:28 +0530

    Adding pages and templates

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class PageTests(SimpleTestCase):
+    def test_page_home_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_page_about_status(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,11 @@
 #pages/urls.py
-from django.urls import path,include
-from .views import homePageView
-from django.contrib import admin
-
+from django.urls import path
+from .views import HomePageView,AboutPageView
 
 #These are the patterns which are kinda exchangeable
 #If user types nothing, call homePageView and name it "home"
 urlpatterns = [
-    path('',homePageView,name='home'), 
+    path('',HomePageView.as_view(),name='home'), 
+    path('about/',AboutPageView.as_view(),name='about')
 ]
 
